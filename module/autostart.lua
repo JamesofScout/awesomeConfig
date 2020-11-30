f825f69a72d334b59d7dac5aa2550914da688615
Florian Schubert <jamesofscout@gmail.com>
2020-11-30 19:07:54 +0100

    Completed Autostart Module

--- module/autostart.lua
+++ module/autostart.lua
@@ -1,10 +1,9 @@
 local lunajson = require 'lunajson'
 
-local config = io.open("config/autostart.json", "r")
+local config = io.open(".config/awesome/config/autostart.json", "r")
 local awful = require("awful")
-
 cmds = lunajson.decode(config:read("*a"))
 
-for _,cmd in cmds do
-    awful.spawn.with_shell(cmd);
+for i=1,#cmds do
+    awful.spawn.with_shell(cmds[i]);
 end
