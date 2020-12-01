d4ca98bc3a51d463d26ca2502ca9f1f5a6cd92cb
Florian Schubert <jamesofscout@gmail.com>
2020-12-01 23:53:04 +0100

    Corrected Misstake

--- layout/taskbar.lua
+++ layout/taskbar.lua
@@ -4,45 +4,47 @@ local wibox = require('wibox')
 local gears = require('gears')
 local dpi = require('beautiful').xresources.apply_dpi
 
-local tasklist = awful.widget.tasklist {
-    screen   = s,
-    filter   = awful.widget.tasklist.filter.currenttags,
-    buttons  = tasklist_buttons,
-    layout   = {
-        spacing_widget = {
-            {
-                forced_width  = 5,
-                forced_height = 24,
-                thickness     = 1,
-                color         = '#777777',
-                widget        = wibox.widget.separator
-            },
-            valign = 'center',
-            halign = 'center',
-            widget = wibox.container.place,
-        },
-        spacing = 1,
-        layout  = wibox.layout.fixed.horizontal
-    },
-    -- Notice that there is *NO* wibox.wibox prefix, it is a template,
-    -- not a widget instance.
-    widget_template = {
-        {
-            wibox.widget.base.make_widget(),
-            forced_height = 5,
-            id            = 'background_role',
-            widget        = wibox.container.background,
-        },
-        {
-            awful.widget.clienticon,
-            margins = 5,
-            widget  = wibox.container.margin
-        },
-        layout = wibox.layout.align.vertical,
-    },
-}
+
 
 local function TaskList(scr, offset)
+    local tasklist = awful.widget.tasklist {
+        screen   = scr,
+        filter   = awful.widget.tasklist.filter.currenttags,
+        buttons  = tasklist_buttons,
+        layout   = {
+            spacing_widget = {
+                {
+                    forced_width  = 5,
+                    forced_height = 24,
+                    thickness     = 1,
+                    color         = '#777777',
+                    widget        = wibox.widget.separator
+                },
+                valign = 'center',
+                halign = 'center',
+                widget = wibox.container.place,
+            },
+            spacing = 1,
+            layout  = wibox.layout.fixed.horizontal
+        },
+        -- Notice that there is *NO* wibox.wibox prefix, it is a template,
+        -- not a widget instance.
+        widget_template = {
+            {
+                wibox.widget.base.make_widget(),
+                forced_height = 5,
+                id            = 'background_role',
+                widget        = wibox.container.background,
+            },
+            {
+                awful.widget.clienticon,
+                margins = 5,
+                widget  = wibox.container.margin
+            },
+            nil,
+            layout = wibox.layout.align.vertical,
+        },
+    }
     if offset then 
         offsetY = dpi(12) 
         offsetX = dpi(10)
